c3947dfc9e54bd6d0fbecb74c5d3cb1060c0282e
donpushme <midabrey@gmail.com>
2025-06-28 00:04:15 +0900

    delete some media

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -2,14 +2,15 @@ import { useQuery } from "@tanstack/react-query"
 
 import { ApiService } from "@/lib/ApiService"
 
-export const useCharacter = (characterId: string) => {
+export const useCharacter = (characterId: string, enableInterval: boolean = false) => {
     const {data, isLoading, error} = useQuery({
         queryKey: ['character', characterId],
         queryFn: async () => {
             const response = await ApiService.getInstance().getCharacterById(characterId)
             return response
         },
-        enabled: !!characterId
+        enabled: !!characterId,
+        refetchInterval: enableInterval ? 40000 : false
     })
 
     return {data, isLoading, error}
